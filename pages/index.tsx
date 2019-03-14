f76be15629dd4539ee9da81ae41775dcc32a7da2
MehriGolchin <mehri.GOLCHIN-KHARAZI@etu.univ-amu.fr>
2019-03-14 22:06:38 +0100

    Add page

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { NextStatelessComponent } from "next";
+ import Header from "../components/Header"
+
+const Index = () => (
+  <div>
+    <div>
+      <Header/>
+    </div>
+    <div>
+      logo
+    </div>
+    <div>
+      KANSUITE
+    </div>
+    <div>
+      EVENT-DRIVEN SERVERLESS PLATFORM
+    </div>
+  </div>
+  
+)
+
+export default Index
