df5cc1ca98905020d3bd1a0dc61d199e16102448
mehdigolchin <mehdi.golchin.kharazi@gmail.com>
2019-03-17 10:31:58 +0100

    Add typography

---
+++ components/typography/typography.styles.ts
@@ -0,0 +1,19 @@
+import styled, { css } from 'styled-components';
+
+export interface TypographyProps {
+    readonly upperCase?: boolean;
+}
+
+export const Title = styled('h1') <TypographyProps>`
+    color: ${(props) => props.color};
+    ${(props) => props.upperCase && css`
+        text-transform: uppercase;
+    `}
+`;
+
+export const Subtitle = styled('h3') <TypographyProps>`
+    color: ${(props) => props.color};
+    ${(props) => props.upperCase && css`
+        text-transform: uppercase;
+    `}
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
-import { Logo, SubTitle, Title, Row, Column } from '../components';
+import { Logo, Subtitle, Title, Row, Column } from '../components';
 
 const App = () => (
   <Row cover vertical>
     <Column grow={2} gutter={50}>
       <Logo />
     </Column>
-    <Column>
-      <Title color="#fff" fontFamily="Tahoma" upperCase>kansuite</Title>
-      <SubTitle color="#808080" fontFamily="Tahoma" upperCase>event-driven serverless platform</SubTitle>
+    <Column grow={1} justify="center">
+      <Title upperCase>kansuite</Title>
+      <Subtitle upperCase>event-driven serverless platform</Subtitle>
     </Column>
   </Row>
 )
